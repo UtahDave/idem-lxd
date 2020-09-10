3e6446ca797fa1c944483313c0de0d6202a44b37
David Boucha <boucha@gmail.com>
2020-09-10 12:39:03 -0600

    moar tests

--- tests/integration/lxd/exec/lxd/test_containers.py
+++ tests/integration/lxd/exec/lxd/test_containers.py
@@ -11,6 +11,27 @@ async def test_create(hub, ctx):
 
 
 @pytest.mark.asyncio
+async def test_get(hub, ctx):
+    container = await hub.exec.lxd.containers.get(ctx, "testlxdcreate")
+    assert "amd64" in container["testlxdcreate"]["image.architecture"]
+    assert "Centos" in container["testlxdcreate"]["image.os"]
+
+
+@pytest.mark.asyncio
+async def test_start(hub, ctx):
+    container = await hub.exec.lxd.containers.start(ctx, "testlxdcreate", wait=True)
+    status = await hub.exec.lxd.containers.status(ctx, "testlxdcreate")
+    assert "Running" in status["status"]
+
+
+@pytest.mark.asyncio
+async def test_stop(hub, ctx):
+    container = await hub.exec.lxd.containers.stop(ctx, "testlxdcreate", wait=True)
+    status = await hub.exec.lxd.containers.status(ctx, "testlxdcreate")
+    assert "Stopped" in status["status"]
+
+
+@pytest.mark.asyncio
 async def test_delete(hub, ctx):
     container = await hub.exec.lxd.containers.delete(ctx, "testlxdcreate")
     assert "Deleting container: testlxdcreate" in container["status"]
