4a83d3e8601246f0f5a0db158886b2c0a4310a6c
David Boucha <boucha@gmail.com>
2020-09-10 18:49:08 -0600

    Update container tests to use Alpine container
    This smaller container image will speed up tests

--- tests/integration/lxd/exec/lxd/test_containers.py
+++ tests/integration/lxd/exec/lxd/test_containers.py
@@ -3,10 +3,14 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_create(hub, ctx):
-    # currently this is hard coded to use an image named 'centos7'
-    # You must have a local image with an alias of 'centos7' for this test to pass
+    # currently this is hard coded to download and use the "alpine/3.11" image.
     # TODO: create a fixture to make sure the correct image exists
-    container = await hub.exec.lxd.containers.create(ctx, "testlxdcreate", "centos7")
+    image = await hub.exec.lxd.images.copy_from(ctx, "alpine/3.11")
+    assert "alpine/3.11" in image
+
+    container = await hub.exec.lxd.containers.create(
+        ctx, "testlxdcreate", "alpine/3.11"
+    )
     assert "Creating container: testlxdcreate" in container["status"]
 
 
@@ -14,7 +18,7 @@ async def test_create(hub, ctx):
 async def test_get(hub, ctx):
     container = await hub.exec.lxd.containers.get(ctx, "testlxdcreate")
     assert "amd64" in container["testlxdcreate"]["image.architecture"]
-    assert "Centos" in container["testlxdcreate"]["image.os"]
+    assert "Alpine" in container["testlxdcreate"]["image.os"]
 
 
 @pytest.mark.asyncio
@@ -35,3 +39,5 @@ async def test_stop(hub, ctx):
 async def test_delete(hub, ctx):
     container = await hub.exec.lxd.containers.delete(ctx, "testlxdcreate")
     assert "Deleting container: testlxdcreate" in container["status"]
+    image = await hub.exec.lxd.images.delete(ctx, "alpine/3.11", wait=True)
+    assert "has been deleted" in image["status"]
